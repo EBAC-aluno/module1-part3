34d76d272fd7d1c586a290a8060bfde3473afa3d
loboph <lobophf@gmail.com>
2023-03-26 22:02:31 -0300

    first commit

---
+++ Application.java
@@ -0,0 +1,18 @@
+public class Application {
+  public static void main(String[] args) {
+
+    Stack s = new Stack();
+    s.push(6);
+    s.push(53);
+    s.push(3);
+    s.push(7);
+    s.push(22);
+    s.push(9);
+    s.push(1);
+
+    while (!s.isEmpty()) {
+      System.out.println(s.pop());
+    }
+
+  }
+}
